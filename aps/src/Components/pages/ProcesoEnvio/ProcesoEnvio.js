c5c1c5d1987f2a13bfaa2a31c2bd54ed4c9cd610
Mauricio Basurto <za18011057@zapopan.tecmm.edu.mx>
2021-11-17 17:21:30 -0600

    commit

---
+++ aps/src/Components/pages/ProcesoEnvio/ProcesoEnvio.js
@@ -0,0 +1,129 @@
+import React, {useEffect, useState} from 'react'
+
+export default function ProcesoEnvio() {
+
+    const [direccion, setDireccion] = useState({
+        calle:'',
+        next:'',
+        nint: '',
+        colonia:'',
+        ciudad:'',
+        cp:'',
+        estado:''
+    })
+
+    const submitNewAddress = (e) => {
+        e.preventDefault();
+        const addr = `${direccion.calle} ${direccion.next} ${direccion.nint}`;
+        const body = JSON.stringify({
+            direccion: addr,
+            colonia: direccion.colonia,
+            ciudad :direccion.ciudad,
+            estado : direccion.estado,
+            cp: direccion.cp
+        });
+        console.log(body);
+
+    }
+
+    const pagos = JSON.parse(localStorage.getItem('paymentInfo'));
+    const onChange = (e) => {
+        setDireccion({...direccion, [e.target.name] : e.target.value})
+    }
+
+    return (
+        <div className='m'>
+            <main
+                className='cont d-flex flex-sm-column flex-md-column flex-lg-row  justify-content-between p-5'>
+                <section className='w-100'>
+                    <h2 className='display-2 fw-bold text-uppercase mb-5'>Elige una de tus direcciones</h2>
+                </section>
+                <p>o</p>
+                <aside className='w-100'>
+                    <h2 className='display-2 fw-bold text-uppercase mb-5'>Agrega una nueva direccion</h2>
+                    <form className='formulario' onSubmit={e => submitNewAddress(e)}>
+                        <fieldset className='formulario__fieldset'>
+                            <legend>Direccion</legend>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='calle' className='m-2'>Calle</label>
+                                <input
+                                    id='calle'
+                                    name='calle'
+                                    required='required'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='text'></input>
+                            </div>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='next' className='m-2'>Numero Exterior</label>
+                                <input
+                                    id='next'
+                                    name='next'
+                                    required='required'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='text'></input>
+                            </div>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='nint' className='m-2'>Numero Interior *</label>
+                                <input
+                                    id='nint'
+                                    name='nint'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='text'></input>
+                            </div>
+                        </fieldset>
+                        <fieldset className='formulario__fieldset'>
+                            <legend>Ubicacion</legend>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='colonia' className='m-2'>Colonia</label>
+                                <input
+                                    id='colonia'
+                                    name='colonia'
+                                    required='required'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='text'></input>
+                            </div>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='ciudad' className='m-2'>Ciudad</label>
+                                <input
+                                    id='ciudad'
+                                    name='ciudad'
+                                    required='required'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='text'></input>
+                            </div>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='cp' className='m-2'>CP</label>
+                                <input
+                                    id='cp'
+                                    name='cp'
+                                    required='required'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='number'
+                                    min='00001'
+                                    max='99999'></input>
+                            </div>
+                            <div className='formulario__input d-flex justify-content-between'>
+                                <label for='estado' className='m-2'>Estado</label>
+                                <input
+                                    id='estado'
+                                    name='estado'
+                                    required='required'
+                                    onChange={e => onChange(e)}
+                                    className='w-100'
+                                    type='text'
+                                    ></input>
+                            </div>
+                        </fieldset>
+                        <button className='btn btn-lg btn-primary'>Agregar Direccion</button>
+                    </form>
+                </aside>
+            </main>
+        </div>
+    )
+}
